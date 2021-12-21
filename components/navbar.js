3d79689f9de4161d63c6cf9d4a1b654371406cc8
Anthony Magana <anthonymagana0705@gmail.com>
2021-12-21 02:31:35 -0800

    updates responsive design

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,32 @@
-import { useColorMode, IconButton, Box, Heading, Link } from '@chakra-ui/react'
+import { 
+    useColorMode, 
+    IconButton, 
+    Box, 
+    Heading, 
+    Link, 
+    Drawer,
+    DrawerBody,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    useMediaQuery
+} from '@chakra-ui/react'
 import NextLink from "next/link"
-import {MoonIcon, SunIcon} from '@chakra-ui/icons'
+import {MoonIcon, SunIcon, HamburgerIcon} from '@chakra-ui/icons'
 import { useUser } from '@auth0/nextjs-auth0';
 
 function Navbar() {
 
     const { colorMode, toggleColorMode } = useColorMode()
     const { user, error, isLoading } = useUser();
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [isMobile] = useMediaQuery('(max-width: 762px)')
+
+    const handleClick = () => {
+        onOpen()
+    }
 
     if(isLoading) {
         return <div>Loading...</div>
@@ -19,28 +39,61 @@ function Navbar() {
         <header>
             <Box w='100%' display='flex' justifyContent='space-between' style={{padding: '10px 0 10px 0', width: '75%', margin: '0 auto'}}>
                 <Heading as='h1' size='xl'>Jobs Jet</Heading>
-                <div>
-                    <IconButton onClick={toggleColorMode} variant="outline" aria-label='toggle color mode' icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} style={{marginRight: '15px'}} />
-                    <NextLink href='/' passHref>
-                        <Link style={{paddingRight: '15px'}}>Home</Link>
-                    </NextLink>
-                    <NextLink href='/about' passHref>
-                        <Link style={{paddingRight: '15px'}}>About</Link>
-                    </NextLink>
-                    <NextLink href='/contact' passHref>
-                        <Link style={{paddingRight: '15px'}}>Contact</Link>
-                    </NextLink>
-                    {!user ?
-                        (<NextLink href='/api/auth/login' passHref>
-                            <Link>Login</Link>
+                {!isMobile &&
+                    <div>
+                        <IconButton onClick={toggleColorMode} variant="outline" aria-label='toggle color mode' icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} style={{marginRight: '15px'}} />
+                        <NextLink href='/' passHref>
+                            <Link style={{paddingRight: '15px'}}>Home</Link>
                         </NextLink>
-                        ) :
-                        (<NextLink href='/api/auth/logout' passHref>
-                            <Link>Logout</Link>
+                        <NextLink href='/about' passHref>
+                            <Link style={{paddingRight: '15px'}}>About</Link>
                         </NextLink>
-                        )
-                    }
-                </div>
+                        <NextLink href='/contact' passHref>
+                            <Link style={{paddingRight: '15px'}}>Contact</Link>
+                        </NextLink>
+                        {!user ?
+                            (<NextLink href='/api/auth/login' passHref>
+                                <Link>Login</Link>
+                            </NextLink>
+                            ) :
+                            (<NextLink href='/api/auth/logout' passHref>
+                                <Link>Logout</Link>
+                            </NextLink>
+                            )
+                        }
+                    </div>
+                }
+                { isMobile &&
+                    <IconButton onClick={() => handleClick()} m={4} icon={ <HamburgerIcon />} m='0' />
+                }
+                <Drawer onClose={onClose} isOpen={isOpen} size='xs'>
+                    <DrawerOverlay />
+                    <DrawerContent>
+                        <DrawerCloseButton mt='10px' />
+                        <DrawerHeader>{colorMode} mode: <IconButton onClick={toggleColorMode} ml='15px' variant="outline" aria-label='toggle color mode' icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} /></DrawerHeader>
+                        <DrawerBody display='flex' flexDir='column' alignItems='center' textAlign='center' justifyContent='center'>
+                            <NextLink href='/' passHref>
+                                <Link onClick={onClose} fontSize='xl' mb='5'>Home</Link>
+                            </NextLink>
+                            <NextLink href='/about' passHref>
+                                <Link onClick={onClose} fontSize='xl' mb='5' >About</Link>
+                            </NextLink>
+                            <NextLink href='/contact' passHref>
+                                <Link onClick={onClose} fontSize='xl' mb='5'>Contact</Link>
+                            </NextLink>
+                            {!user ?
+                                (<NextLink href='/api/auth/login' passHref>
+                                    <Link onClick={onClose} fontSize='xl' mb='5' >Login</Link>
+                                </NextLink>
+                                ) :
+                                (<NextLink href='/api/auth/logout' passHref>
+                                    <Link onClick={onClose} fontSize='xl' mb='5' >Logout</Link>
+                                </NextLink>
+                                )
+                            }
+                        </DrawerBody>
+                    </DrawerContent>
+                </Drawer>
             </Box>
         </header>
     )
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import Image from 'next/image'
 import NextLink from "next/link"
-import { Heading, Box, Text, Button, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Text, Button, Grid, GridItem } from '@chakra-ui/react'
 import { AddIcon, StarIcon, UnlockIcon } from '@chakra-ui/icons'
 
 export default function Home() {
@@ -14,11 +14,11 @@ export default function Home() {
       </Head>
       <div className={styles.container}>
         <Box display='flex' justifyContent='space-between'>
-          <div>
-            <Heading as='h1' maxW='md' size='xl' style={{marginBottom: '20px'}}>Find the perfect match for you with just a swipe!</Heading>
-            <Text style={{marginBottom: '15px'}}>Match employers or talent based on field category, start now.</Text>
+          <Box mr='10px'>
+            <Box fontWeight='bold' as='h1' maxW='md' fontSize={['lg', '2xl', '3xl']} style={{marginBottom: '20px'}}>Find the perfect match for you with just a swipe</Box>
+            <Text fontSize={['sm', 'md']} style={{marginBottom: '15px'}}>Match employers or talent based on field category, start now.</Text>
             <Button><NextLink href="/api/auth/login">Sign Up</NextLink></Button>
-          </div>
+          </Box>
           <Image 
             src="/homeimg.png"
             alt="Phone images"
@@ -26,42 +26,44 @@ export default function Home() {
             height={300} 
           />
         </Box>
-        <Box style={{marginTop: '100px', marginBottom: '150px'}}>
-          <Text color='purple.500' fontSize='lg' fontWeight='bold' align='center' style={{marginBottom: '5px'}}>Our Services</Text>
-          <Text fontSize='3xl' align='center'>We provide the Best Quality Services</Text>
-          <Text fontSize='md' align='center' maxW='xl' margin='0 auto' style={{marginTop: '10px', marginBottom: '45px'}}>
+        <Box style={{marginTop: '100px'}} mb={{base:'350px', sm: '325px', md: '100px', lg: '100px'}}>
+          <Text color='purple.500' fontSize={['md','lg']} fontWeight='bold' align='center' style={{marginBottom: '5px'}}>Our Services</Text>
+          <Text fontSize={['2xl', '3xl']} align='center'>We provide the Best Quality Services</Text>
+          <Text fontSize={['sm', 'md']} align='center' maxW='xl' margin='0 auto' style={{marginTop: '10px', marginBottom: '45px'}}>
             Our commitment to quality ensures that our applications and services get the best functionality and user experience possible. Jobs Jet is more 
             than just a web app, but the entire job hiring experience.
           </Text>
           <Grid
-            h='350px'
+            h={['300px','350px']}
             templateRows='repeat(1, 1fr)'
             templateColumns='repeat(3, 1fr)'
             gap={6}
+            display={{ md: 'flex' }}
+            mb={{base: '300px', sm: '150px', md: '100px' }}
           >
-            <GridItem rowSpan={1} colSpan={1} bg='orange.400' boxShadow='2xl'>
-              <Box display='flex' flexDir='column' justifyContent='center' alignItems='center' margin='90px 0 0 0'>
-                <AddIcon fontSize='3xl' color='white' margin='0px 0 25px 0' />
-                <Text fontWeight='bold' fontSize='lg' color='white'>Jobs Jet Plus</Text>
-                <Text fontSize='md' color='white' textAlign='center' padding='15px 0'>
+            <GridItem rowSpan={1} colSpan={1} bg='orange.400' boxShadow='2xl' mb={{ base: '20px', sm: '20px', md: '0px', lg: '0px'}}>
+              <Box display='flex' flexDir='column' justifyContent='center' alignItems='center' pt={{base: '25px', sm: '25px', md: '64px', lg: '80px'}}>
+                <AddIcon fontSize={['2xl','3xl']} color='white' margin='0px 0 25px 0' />
+                <Text fontWeight='bold' fontSize={['sm','md','lg']} color='white' align='center'>Jobs Jet Plus</Text>
+                <Text fontSize={['xs','sm','md']} color='white' textAlign='center' padding='15px 5px'>
                   Choosing who to Like can be tough, so we made it easier with Unlimited Likes. Send unlimited Likes to increase your match-making potential.
                 </Text>
               </Box>
             </GridItem>
-            <GridItem rowSpan={1} colSpan={1} bg='teal.400' boxShadow='2xl'>
-              <Box display='flex' flexDir='column' justifyContent='center' alignItems='center' margin='90px 0 0 0'>
-                <StarIcon fontSize='3xl' color='white' margin='0px 0 25px 0' />
-                <Text fontWeight='bold' fontSize='lg' color='white' align='center'>Jobs Jet Gold</Text>
-                <Text fontSize='md' color='white' textAlign='center' padding='15px 5px'>
+            <GridItem rowSpan={1} colSpan={1} bg='teal.400' boxShadow='2xl' mb={{base: '20px', sm: '20px', md: '0px', lg: '0px'}}>
+              <Box display='flex' flexDir='column' justifyContent='center' alignItems='center' pt={{base: '25px', lg: '80px', md: '64px', sm: '25px'}}>
+                <StarIcon fontSize={['2xl','3xl']} color='white' margin='0px 0 25px 0' />
+                <Text fontWeight='bold' fontSize={['sm','md','lg']} color='white' align='center'>Jobs Jet Gold</Text>
+                <Text fontSize={['xs','sm','md']} color='white' textAlign='center' padding='15px 5px'>
                 Jobs Jet Gold saves time by letting you see who Likes you. Match, pass, and expand photos to view full profiles with a simple tap and get more efficient with your time online.
                 </Text>
               </Box>
             </GridItem>
-            <GridItem rowSpan={1} colSpan={1} bg='purple.400' boxShadow='2xl'>
-              <Box display='flex' flexDir='column' justifyContent='center' alignItems='center' margin='90px 0 0 0'>
-                <UnlockIcon fontSize='3xl' color='white' margin='0px 0 25px 0' />
-                <Text fontWeight='bold' fontSize='lg' color='white' align='center'>Jobs Jet Platinum</Text>
-                <Text fontSize='md' color='white' textAlign='center' padding='15px 5px'>
+            <GridItem rowSpan={1} colSpan={1} bg='purple.400' boxShadow='2xl' mb={{base: '20px', sm: '20px', md: '0px', lg: '0px'}}>
+              <Box display='flex' flexDir='column' justifyContent='center' alignItems='center' pt={{base: '25px', lg: '80px', md: '64px', sm: '25px'}}>
+                <UnlockIcon fontSize={['2xl','3xl']} color='white' margin='0px 0 25px 0' />
+                <Text fontWeight='bold' fontSize={['sm','md','lg']} color='white' align='center'>Jobs Jet Platinum</Text>
+                <Text fontSize={['xs','sm','md']} color='white' textAlign='center' padding='15px 5px'>
                   Enjoy all of Jobs Jet&apos;s premium features while getting maximum visibility on the app. Jobs Jet Platinum is the ultimate job-finding service.
                 </Text>
               </Box>
